94bb9e7a6ea815b7611e58b4e2ae92089fe9a7eb
Joshua Miller <jshuasmiller@gmail.com>
2013-04-30 23:17:48 -0500

    for beks

--- beka.py
+++ beka.py
@@ -1,10 +1,16 @@
 
 def ShouldIStay(Neighborcolors, p):
+    
+    # Find the number of times each color occurs
     numG = Neighborcolors.count("G")
     numR = Neighborcolors.count("R")
+    
+    # Calculate percentages of each color
     lenColor = len(Neighborcolors)
     percG = float(numG)/lenColor*100
     percR = float(numR)/lenColor*100
+
+    # Compare to what we want
     if percR < p or percG < p:
         print "Get out of here"
     else:
@@ -12,4 +18,9 @@ def ShouldIStay(Neighborcolors, p):
 
 Neighborcolors = [ "R", "R", "G", "R", "B", "R", "G", "B", "B", "G", "R", "G", "G"]
 p = input("Enter a number between 0 and 100 for the percent: ")
+
+#################
+#      Run  
+#################
+
 ShouldIStay(Neighborcolors, p)
